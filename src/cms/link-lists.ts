03fd186adeb178a17115b340e9e69fead859a277
Thomas Curran <39130169+tc-amorevino@users.noreply.github.com>
2025-05-23 16:24:30 +0200

    chore(ux): content updates

---
+++ src/cms/link-lists.ts
@@ -0,0 +1,55 @@
+export const footerLinksSite = [
+  {
+    title: 'sitemap.xml',
+    href: `/sitemap-index.xml`,
+  },
+  {
+    title: 'robots.txt',
+    href: `/robots.txt`,
+  },
+  {
+    title: 'humans.txt',
+    href: `/human.txt`,
+  },
+  {
+    title: 'ai.txt',
+    href: `/ai.txt`,
+  },
+];
+export const footerLinksSocial = [
+  {
+    title: 'GitHub',
+    href: 'https://github.com/tacurran',
+  },
+  {
+    title: 'LinkedIn',
+    href: 'https://www.linkedin.com/in/thomasaidancurran/',
+  },
+  {
+    title: 'X',
+    href: 'https://x.com/tacurran',
+  },
+];
+
+export const headerLinksNav = [
+  {
+    title: 'About',
+    href: `/`, // home root
+  },
+  {
+    title: 'Publications',
+    href: `/publications`,
+  },
+  // {
+  //   title: 'Blog',
+  //   href: `/blog`,
+  // },
+  // {
+  //   title: 'Portfolio',
+  //   href: `/portfolio`,
+  // },
+  {
+    title: 'Inquiries',
+    href: `/inquiries`,
+  },
+];
---
+++ src/content.config.ts
@@ -0,0 +1,12 @@
+import { defineCollection, z } from 'astro:content';
+
+import { glob } from 'astro/loaders';
+
+const blog = defineCollection({
+  loader: glob({ pattern: '**/*.mdx', base: './src/cms/blog' }),
+  schema: z.object({
+    title: z.string(),
+  }),
+});
+
+export const collections = { blog };
