c36ce7456427183e1c3ab09488fec6cfa36adaf9
Thomas Curran <39130169+tc-amorevino@users.noreply.github.com>
2026-07-09 14:10:34 +0200

    revert: site URL configuration in astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -1,35 +1,13 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig, envField, fontProviders } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import htmlBeautifier from 'astro-html-beautifier';
-import { loadEnv } from 'vite';
-
-// Manually load the environment into the config, as Astro by default exposes
-// the environment variables only after processing the configuration file during
-// the build step. We know certain environment variables will be available as
-// they are validated during build time.
-// https://main.vite.dev/config/#using-environment-variables-in-config
-const hotEnv = loadEnv(process.env.NODE_ENV ?? '', process.cwd(), 'PUBLIC_');
-const defaultSiteUrl = 'https://thomasaidancurran.com/';
 
 export default defineConfig({
-  site: hotEnv.PUBLIC_SITE_URL || defaultSiteUrl,
-
+  site: 'https://thomasaidancurran.com/',
   devToolbar: {
     enabled: false,
-  },
-
-  env: {
-    schema: {
-      PUBLIC_SITE_URL: envField.string({
-        context: 'client',
-        access: 'public',
-        optional: false,
-        default: defaultSiteUrl,
-      }),
-    },
-    validateSecrets: true,
   },
   build: {
     assets: '_assets',
