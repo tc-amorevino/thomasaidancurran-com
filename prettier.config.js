ed45a5eb28202733fce727043133d76ec770f7d8
Thomas Curran <39130169+tc-amorevino@users.noreply.github.com>
2026-07-09 12:32:31 +0200

    chore(dx): update prettier config to include packagejson plugin

--- prettier.config.js
+++ prettier.config.js
@@ -3,8 +3,12 @@
  * @see https://prettier.io/docs/en/configuration.html
  * @type {import("prettier").Config}
  */
-const config = {
-  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+export default {
+  plugins: [
+    'prettier-plugin-astro',
+    'prettier-plugin-tailwindcss',
+    'prettier-plugin-packagejson',
+  ],
   tailwindConfig: './tailwind.config.ts',
   tailwindFunctions: ['clsx', 'classnames', 'ctl'],
   singleQuote: true,
@@ -19,6 +23,13 @@ const config = {
       files: ['*.astro'],
       options: {
         parser: 'astro',
+        bracketSameLine: true,
+      },
+    },
+    {
+      files: './src/styles/global.css',
+      options: {
+        printWidth: 120,
       },
     },
     {
@@ -31,5 +42,3 @@ const config = {
     },
   ],
 };
-
-export default config;
